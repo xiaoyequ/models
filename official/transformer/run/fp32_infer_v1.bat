df13554cee4ba959865ebf712ebfbe559a43c5f1
Xiaoming (Jason) Cui <xiaoming.cui@intel.com>
2019-10-23 18:42:30 -0700

    Added the script to run the model with python3 mkl optimized tensorflow r1.15

---
+++ official/transformer/run/fp32_infer_v1.bat
@@ -0,0 +1,28 @@
+# Ensure that PYTHONPATH is correctly defined as described in
+# https://github.com/tensorflow/models/tree/master/official#requirements
+# works for TensorFlow r1.15
+
+# Export variables
+PARAM_SET=big
+
+ROOTDIR=/nfs/pdx/home/cuixiaom
+LOCALDIR=/localdisk/cuixiaom
+export SRCDIR=$ROOTDIR/Src/xiaoyequ/models
+export TRANFORMERV1DIR=$SRCDIR/official/transformer
+export PYTHONPATH="$PYTHONPATH:$SRCDIR"
+DATA_DIR=$ROOTDIR/Data/transformer/data
+MODEL_DIR=$ROOTDIR/Output/Transformer/model_$PARAM_SET
+VOCAB_FILE=$DATA_DIR/vocab.ende.32768
+
+OUTPUT_DIR=$ROOTDIR/Output/Transformer/Results
+
+cd $TRANFORMERV1DIR
+
+# Translate some text using the trained model
+python3 translate.py --model_dir=$MODEL_DIR --vocab_file=$VOCAB_FILE \
+    --param_set=$PARAM_SET --text="hello world"
+
+# Compute model's BLEU score using the newstest2014 dataset.
+python3 translate.py --model_dir=$MODEL_DIR --vocab_file=$VOCAB_FILE \
+    --param_set=$PARAM_SET --file=$DATA_DIR/newstest2014.en --file_out=$OUTPUT_DIR/translation.en
+python3 compute_bleu.py --translation=$OUTPUT_DIR/translation.en --reference=$DATA_DIR/newstest2014.de
---
+++ official/transformer/run/fp32_train_v1.bat
@@ -0,0 +1,22 @@
+# Ensure that PYTHONPATH is correctly defined as described in
+# https://github.com/tensorflow/models/tree/master/official#requirements
+# works for TensorFlow python3.6 mkl r1.15
+
+# Export variables
+PARAM_SET=big
+
+ROOTDIR=/nfs/pdx/home/cuixiaom
+LOCALDIR=/localdisk/cuixiaom
+export SRCDIR=$ROOTDIR/Src/xiaoyequ/models
+export TRANFORMERV1DIR=$SRCDIR/official/transformer
+export PYTHONPATH="$PYTHONPATH:$SRCDIR"
+DATA_DIR=$LOCALDIR/Data/transformer/data
+MODEL_DIR=$LOCALDIR/Output/Transformer/model_$PARAM_SET
+VOCAB_FILE=$DATA_DIR/vocab.ende.32768
+
+cd $TRANFORMERV1DIR
+
+# Train the model for 10 epochs, and evaluate after every epoch.
+python3 transformer_main.py --data_dir=$DATA_DIR --model_dir=$MODEL_DIR --param_set=$PARAM_SET 
+#    --vocab_file=$VOCAB_FILE \
+#    --bleu_source=$DATA_DIR/newstest2014.en --bleu_ref=$DATA_DIR/newstest2014.de
