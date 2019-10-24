80dae16fd9317b76835700f7dd1a9778f8683a5d
Xiaoming (Jason) Cui <xiaoming.cui@intel.com>
2019-10-23 19:00:25 -0700

    move the data output dir to nfs directory instead of local dir

--- official/transformer/run/fp32_train_v1.bat
+++ official/transformer/run/fp32_train_v1.bat
@@ -10,13 +10,13 @@ LOCALDIR=/localdisk/cuixiaom
 export SRCDIR=$ROOTDIR/Src/xiaoyequ/models
 export TRANFORMERV1DIR=$SRCDIR/official/transformer
 export PYTHONPATH="$PYTHONPATH:$SRCDIR"
-DATA_DIR=$LOCALDIR/Data/transformer/data
-MODEL_DIR=$LOCALDIR/Output/Transformer/model_$PARAM_SET
+DATA_DIR=$ROOTDIR/Data/transformer/data
+MODEL_DIR=$ROOTDIR/Output/Transformer/model_$PARAM_SET
 VOCAB_FILE=$DATA_DIR/vocab.ende.32768
 
 cd $TRANFORMERV1DIR
 
 # Train the model for 10 epochs, and evaluate after every epoch.
 python3 transformer_main.py --data_dir=$DATA_DIR --model_dir=$MODEL_DIR --param_set=$PARAM_SET 
-#    --vocab_file=$VOCAB_FILE \
-#    --bleu_source=$DATA_DIR/newstest2014.en --bleu_ref=$DATA_DIR/newstest2014.de
+    --vocab_file=$VOCAB_FILE \
+    --bleu_source=$DATA_DIR/newstest2014.en --bleu_ref=$DATA_DIR/newstest2014.de
